d792ef64c205e584117c63fce92b84bcdd983a96
Neil Brennan <nello@anywhich.com>
2023-02-13 16:02:45 +1100

    3262: better wiring for version_uri

--- lib/integrations/true_pillars.rb
+++ lib/integrations/true_pillars.rb
@@ -1,9 +1,7 @@
 require 'api'
 
-VERSION_URI=ENV['VERSION_URI'] || "https://api.truepillars.com/version"
-
 module TruePillars
-  def self.version
-    Api::get(VERSION_URI)['number']
+  def self.version(uri)
+    Api::get(uri)['number']
   end
 end
